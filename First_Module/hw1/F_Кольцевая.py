23e6f1e0299671f0550f5cdeb18f149d15738284
popegorov <116119217+popegorov@users.noreply.github.com>
2023-02-05 22:25:57 +0300

    Delete First_Module/hw1 directory

--- First_Module/hw1/F_Кольцевая.py
+++
@@ -1,9 +0,0 @@
-a = list(map(int, input().split()))
-n = a[0]
-i = a[1]
-j = a[2]
-first = abs(j - i) - 1
-second = n - max(i, j) + min(i, j) - 1
-print(min(first, second))
-
-
--- First_Module/hw1/H_Строительство_школы.py
+++
@@ -1,6 +0,0 @@
-import math
-n = int(input())
-a = sorted(list(map(int, input().split())))
-print(a[len(a) // 2])
-
-
